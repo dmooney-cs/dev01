483e485745fc4bc0fcd2128ba3febcf6d59c7f71
dmooney-cs <dougm@connectsecure.com>
2026-01-02 19:46:47 -0500

    Update CS-Toolbox-DecryptorDesktopIcon.ps1

--- CS-Toolbox-DecryptorDesktopIcon.ps1
+++ CS-Toolbox-DecryptorDesktopIcon.ps1
@@ -21,7 +21,7 @@
 param(
     [Parameter(Mandatory=$false)]
     [ValidateNotNullOrEmpty()]
-    [string]$ZipUrl = "https://github.com/dmooney-cs/dev01/raw/refs/heads/main/Toolbox-Launchers.zip",
+    [string]$ZipUrl = "https://github.com/dmooney-cs/dev01/raw/refs/heads/main/decryptor-zip.zip",
 
     [switch]$Silent,
 
